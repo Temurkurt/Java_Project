50c445f1f766d0258cfcb8c24ac2d17f30b3fbf6
A.Arif Demirel <demirel52arif@gmail.com>
2023-08-26 12:04:12 +0300

    Delete Quadrilateral directory

--- Quadrilateral/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quadrilateral {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quadrilateral to javafx.fxml;
-    exports com.example.quadrilateral;
-}
