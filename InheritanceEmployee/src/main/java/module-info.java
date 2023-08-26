5e090548671f04a2fcf601306add173bc99130d8
A.Arif Demirel <demirel52arif@gmail.com>
2023-08-26 12:02:07 +0300

    Delete InheritanceEmployee directory

--- InheritanceEmployee/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.inheritanceemployee {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.inheritanceemployee to javafx.fxml;
-    exports com.example.inheritanceemployee;
-}
